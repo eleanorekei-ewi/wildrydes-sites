03b9f80b51db4997abd0d71b86b2fa29ac7c7aa0
eleanorekei-ewi <eleanorekeiewi@gmail.com>
2026-03-17 20:55:54 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: '-	us-west-2_QyfH6jOe6', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '-	21bt737achlb37hk58l1n3v262', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
